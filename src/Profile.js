e303d5f7d2ff998e3607bbe57e02011054e9c17b
two2012 <guowen1124@yahoo.com>
2023-03-17 10:13:41 -0700

    initial post

---
+++ src/Profile.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Wen Guo</h1>
+      <h2>Computer Science Bachelor's Degree (In Progress)</h2>
+      <h3>Location: Ontario, CA</h3>
+
+      <h2>Technology Expertise</h2>
+      <ul>
+        <li>Programming Languages: Java, Python, C++, HTML, CSS, JavaScript, R.</li>
+        <li>Database Platforms: MySQL.</li>
+        <li>Libraries: React, Numpy, Pandas.</li>
+        <li>Tools/Apps: IntelliJ IDEA, VS Code, Jupyter, R Studio, CorelDRAW, Office Suite.</li>
+      </ul>
+
+      <h2>Academic Projects (Java)</h2>
+      <ul>
+        <li>Hotel Reservation Application</li>
+        <li>Simple Reddit</li>
+        <li>Airline Reservation</li>
+      </ul>
+
+      <h2>Volunteer Experience</h2>
+      <ul>
+        <li>United Proud Women</li>
+        <ul>
+          <li>Member of the web application team</li>
+          <li>Conduct layout design and construction for a variety of websites as assigned.</li>
+          <li>Provide technical support to other web design team members as directed.</li>
+        </ul>
+      </ul>
+
+      <h2>Work Experience</h2>
+      <ul>
+        <li>United States Navy (Nov 2015 - Jun 2020)</li>
+        <ul>
+          <li>Stationed at Norfolk Naval Station, VA (CVN-78)</li>
+          <li>Established an engraving program and was in charge of the engraving shop.</li>
+          <li>Supervised operation and organizational maintenance of engraving systems and specialized equipment.</li>
+          <li>Planned and estimated projects, assigned daily projects to junior sailors, completed special projects, and supervised project tracking and reporting.</li>
+          <li>Mentored junior sailors to achieve both personal and work-related goals.</li>
+          <li>Evaluated junior sailors’ performance on a quarterly basis and coached and trained them accordingly, increasing the quality of work and junior sailors’ motivation.</li>
+        </ul>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
